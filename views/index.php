cf374649a38cd39b1808cf4bcf0957178c430128
Bioscent <jhonweakening@gmail.com>
2026-02-04 19:00:47 +0700

    new: predict & sidebar layout

--- views/index.php
+++ views/index.php
@@ -3,7 +3,7 @@
 <html
   lang="en"
   class="layout-menu-fixed layout-compact"
-  data-assets-path="assets/"
+  data-../assets-path="../assets/"
   data-template="vertical-menu-template-free">
 
 <head>
@@ -17,7 +17,7 @@
   <meta name="description" content="" />
 
   <!-- Favicon -->
-  <link rel="icon" type="image/x-icon" href="assets/img/favicon/favicon.ico" />
+  <link rel="icon" type="image/x-icon" href="../assets/img/favicon/favicon.ico" />
 
   <!-- Fonts -->
   <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -26,31 +26,31 @@
     href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
     rel="stylesheet" />
 
-  <link rel="stylesheet" href="assets/vendor/fonts/iconify-icons.css" />
+  <link rel="stylesheet" href="../assets/vendor/fonts/iconify-icons.css" />
 
   <!-- Core CSS -->
-  <!-- build:css assets/vendor/css/theme.css  -->
+  <!-- build:css ../assets/vendor/css/theme.css  -->
 
-  <link rel="stylesheet" href="assets/vendor/css/core.css" />
-  <link rel="stylesheet" href="assets/css/demo.css" />
+  <link rel="stylesheet" href="../assets/vendor/css/core.css" />
+  <link rel="stylesheet" href="../assets/css/demo.css" />
 
   <!-- Vendors CSS -->
 
-  <link rel="stylesheet" href="assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
+  <link rel="stylesheet" href="../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
 
   <!-- endbuild -->
 
-  <link rel="stylesheet" href="assets/vendor/libs/apex-charts/apex-charts.css" />
+  <link rel="stylesheet" href="../assets/vendor/libs/apex-charts/apex-charts.css" />
 
   <!-- Page CSS -->
 
   <!-- Helpers -->
-  <script src="assets/vendor/js/helpers.js"></script>
+  <script src="../assets/vendor/js/helpers.js"></script>
   <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
 
   <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
 
-  <script src="assets/js/config.js"></script>
+  <script src="../assets/js/config.js"></script>
 </head>
 
 <body>
@@ -58,110 +58,13 @@
   <div class="layout-wrapper layout-content-navbar">
     <div class="layout-container">
       <!-- Menu -->
-      <?php include 'views/partials/sidebar.html'; ?>
+      <?php include 'partials/sidebar.html'; ?>
       <!-- / Menu -->
 
       <!-- Layout container -->
       <div class="layout-page">
         <!-- Navbar -->
-
-        <nav
-          class="layout-navbar container-xxl navbar-detached navbar navbar-expand-xl align-items-center bg-navbar-theme"
-          id="layout-navbar">
-          <div class="layout-menu-toggle navbar-nav align-items-xl-center me-4 me-xl-0 d-xl-none">
-            <a class="nav-item nav-link px-0 me-xl-6" href="javascript:void(0)">
-              <i class="icon-base bx bx-menu icon-md"></i>
-            </a>
-          </div>
-
-          <div class="navbar-nav-right d-flex align-items-center justify-content-end" id="navbar-collapse">
-            <!-- Search -->
-            <div class="navbar-nav align-items-center me-auto">
-              <div class="nav-item d-flex align-items-center">
-                <span class="w-px-22 h-px-22"><i class="icon-base bx bx-search icon-md"></i></span>
-                <input
-                  type="text"
-                  class="form-control border-0 shadow-none ps-1 ps-sm-2 d-md-block d-none"
-                  placeholder="Search..."
-                  aria-label="Search..." />
-              </div>
-            </div>
-            <!-- /Search -->
-
-            <ul class="navbar-nav flex-row align-items-center ms-md-auto">
-              <!-- Place this tag where you want the button to render. -->
-              <li class="nav-item lh-1 me-4">
-                <a
-                  class="github-button"
-                  href="https://github.com/themeselection/sneat-bootstrap-html-admin-template-free"
-                  data-icon="octicon-star"
-                  data-size="large"
-                  data-show-count="true"
-                  aria-label="Star themeselection/sneat-html-admin-template-free on GitHub">Star</a>
-              </li>
-
-              <!-- User -->
-              <li class="nav-item navbar-dropdown dropdown-user dropdown">
-                <a
-                  class="nav-link dropdown-toggle hide-arrow p-0"
-                  href="javascript:void(0);"
-                  data-bs-toggle="dropdown">
-                  <div class="avatar avatar-online">
-                    <img src="assets/img/avatars/1.png" alt class="w-px-40 h-auto rounded-circle" />
-                  </div>
-                </a>
-                <ul class="dropdown-menu dropdown-menu-end">
-                  <li>
-                    <a class="dropdown-item" href="#">
-                      <div class="d-flex">
-                        <div class="flex-shrink-0 me-3">
-                          <div class="avatar avatar-online">
-                            <img src="assets/img/avatars/1.png" alt class="w-px-40 h-auto rounded-circle" />
-                          </div>
-                        </div>
-                        <div class="flex-grow-1">
-                          <h6 class="mb-0">John Doe</h6>
-                          <small class="text-body-secondary">Admin</small>
-                        </div>
-                      </div>
-                    </a>
-                  </li>
-                  <li>
-                    <div class="dropdown-divider my-1"></div>
-                  </li>
-                  <li>
-                    <a class="dropdown-item" href="#">
-                      <i class="icon-base bx bx-user icon-md me-3"></i><span>My Profile</span>
-                    </a>
-                  </li>
-                  <li>
-                    <a class="dropdown-item" href="#">
-                      <i class="icon-base bx bx-cog icon-md me-3"></i><span>Settings</span>
-                    </a>
-                  </li>
-                  <li>
-                    <a class="dropdown-item" href="#">
-                      <span class="d-flex align-items-center align-middle">
-                        <i class="flex-shrink-0 icon-base bx bx-credit-card icon-md me-3"></i><span class="flex-grow-1 align-middle">Billing Plan</span>
-                        <span class="flex-shrink-0 badge rounded-pill bg-danger">4</span>
-                      </span>
-                    </a>
-                  </li>
-                  <li>
-                    <div class="dropdown-divider my-1"></div>
-                  </li>
-                  <li>
-                    <a class="dropdown-item" href="javascript:void(0);">
-                      <i class="icon-base bx bx-power-off icon-md me-3"></i><span>Log Out</span>
-                    </a>
-                  </li>
-                </ul>
-              </li>
-              <!--/ User -->
-            </ul>
-          </div>
-        </nav>
-
+        <?php include 'partials/navbar.html'; ?>
         <!-- / Navbar -->
 
         <!-- Content wrapper -->
@@ -185,7 +88,7 @@
                     <div class="col-sm-5 text-center text-sm-left">
                       <div class="card-body pb-0 px-0 px-md-6">
                         <img
-                          src="assets/img/illustrations/man-with-laptop.png"
+                          src="../assets/img/illustrations/man-with-laptop.png"
                           height="175"
                           alt="View Badge User" />
                       </div>
@@ -201,7 +104,7 @@
                         <div class="card-title d-flex align-items-start justify-content-between mb-4">
                           <div class="avatar flex-shrink-0">
                             <img
-                              src="assets/img/icons/unicons/chart-success.png"
+                              src="../assets/img/icons/unicons/chart-success.png"
                               alt="chart success"
                               class="rounded" />
                           </div>
@@ -233,7 +136,7 @@
                         <div class="card-title d-flex align-items-start justify-content-between mb-4">
                           <div class="avatar flex-shrink-0">
                             <img
-                              src="assets/img/icons/unicons/wallet-info.png"
+                              src="../assets/img/icons/unicons/wallet-info.png"
                               alt="wallet info"
                               class="rounded" />
                           </div>
@@ -357,7 +260,7 @@
                       <div class="card-body">
                         <div class="card-title d-flex align-items-start justify-content-between mb-4">
                           <div class="avatar flex-shrink-0">
-                            <img src="assets/img/icons/unicons/paypal.png" alt="paypal" class="rounded" />
+                            <img src="../assets/img/icons/unicons/paypal.png" alt="paypal" class="rounded" />
                           </div>
                           <div class="dropdown">
                             <button
@@ -386,7 +289,7 @@
                       <div class="card-body">
                         <div class="card-title d-flex align-items-start justify-content-between mb-4">
                           <div class="avatar flex-shrink-0">
-                            <img src="assets/img/icons/unicons/cc-primary.png" alt="Credit Card" class="rounded" />
+                            <img src="../assets/img/icons/unicons/cc-primary.png" alt="Credit Card" class="rounded" />
                           </div>
                           <div class="dropdown">
                             <button
@@ -560,7 +463,7 @@
                       <div class="tab-pane fade show active" id="navs-tabs-line-card-income" role="tabpanel">
                         <div class="d-flex mb-6">
                           <div class="avatar flex-shrink-0 me-3">
-                            <img src="assets/img/icons/unicons/wallet.png" alt="User" />
+                            <img src="../assets/img/icons/unicons/wallet.png" alt="User" />
                           </div>
                           <div>
                             <p class="mb-0">Total Balance</p>
@@ -616,7 +519,7 @@
                     <ul class="p-0 m-0">
                       <li class="d-flex align-items-center mb-6">
                         <div class="avatar flex-shrink-0 me-3">
-                          <img src="assets/img/icons/unicons/paypal.png" alt="User" class="rounded" />
+                          <img src="../assets/img/icons/unicons/paypal.png" alt="User" class="rounded" />
                         </div>
                         <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
                           <div class="me-2">
@@ -631,7 +534,7 @@
                       </li>
                       <li class="d-flex align-items-center mb-6">
                         <div class="avatar flex-shrink-0 me-3">
-                          <img src="assets/img/icons/unicons/wallet.png" alt="User" class="rounded" />
+                          <img src="../assets/img/icons/unicons/wallet.png" alt="User" class="rounded" />
                         </div>
                         <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
                           <div class="me-2">
@@ -646,7 +549,7 @@
                       </li>
                       <li class="d-flex align-items-center mb-6">
                         <div class="avatar flex-shrink-0 me-3">
-                          <img src="assets/img/icons/unicons/chart.png" alt="User" class="rounded" />
+                          <img src="../assets/img/icons/unicons/chart.png" alt="User" class="rounded" />
                         </div>
                         <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
                           <div class="me-2">
@@ -661,7 +564,7 @@
                       </li>
                       <li class="d-flex align-items-center mb-6">
                         <div class="avatar flex-shrink-0 me-3">
-                          <img src="assets/img/icons/unicons/cc-primary.png" alt="User" class="rounded" />
+                          <img src="../assets/img/icons/unicons/cc-primary.png" alt="User" class="rounded" />
                         </div>
                         <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
                           <div class="me-2">
@@ -676,7 +579,7 @@
                       </li>
                       <li class="d-flex align-items-center mb-6">
                         <div class="avatar flex-shrink-0 me-3">
-                          <img src="assets/img/icons/unicons/wallet.png" alt="User" class="rounded" />
+                          <img src="../assets/img/icons/unicons/wallet.png" alt="User" class="rounded" />
                         </div>
                         <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
                           <div class="me-2">
@@ -691,7 +594,7 @@
                       </li>
                       <li class="d-flex align-items-center">
                         <div class="avatar flex-shrink-0 me-3">
-                          <img src="assets/img/icons/unicons/cc-warning.png" alt="User" class="rounded" />
+                          <img src="../assets/img/icons/unicons/cc-warning.png" alt="User" class="rounded" />
                         </div>
                         <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
                           <div class="me-2">
@@ -714,43 +617,7 @@
           <!-- / Content -->
 
           <!-- Footer -->
-          <footer class="content-footer footer bg-footer-theme">
-            <div class="container-xxl">
-              <div
-                class="footer-container d-flex align-items-center justify-content-between py-4 flex-md-row flex-column">
-                <div class="mb-2 mb-md-0">
-                  ©
-                  <script>
-                    document.write(new Date().getFullYear());
-                  </script>
-                  , made with ❤️ by
-                  <a href="https://themeselection.com" target="_blank" class="footer-link">ThemeSelection</a>
-                </div>
-                <div class="d-none d-lg-inline-block">
-                  <a
-                    href="https://themeselection.com/item/category/admin-templates/"
-                    target="_blank"
-                    class="footer-link me-4">Admin Templates</a>
-
-                  <a href="https://themeselection.com/license/" class="footer-link me-4" target="_blank">License</a>
-                  <a
-                    href="https://themeselection.com/item/category/bootstrap-admin-templates/"
-                    target="_blank"
-                    class="footer-link me-4">Bootstrap Dashboard</a>
-
-                  <a
-                    href="https://demos.themeselection.com/sneat-bootstrap-html-admin-template/documentation/"
-                    target="_blank"
-                    class="footer-link me-4">Documentation</a>
-
-                  <a
-                    href="https://github.com/themeselection/sneat-bootstrap-html-admin-template-free/issues"
-                    target="_blank"
-                    class="footer-link">Support</a>
-                </div>
-              </div>
-            </div>
-          </footer>
+          <?php include 'partials/footer.html'; ?>
           <!-- / Footer -->
 
           <div class="content-backdrop fade"></div>
@@ -765,35 +632,28 @@
   </div>
   <!-- / Layout wrapper -->
 
-  <div class="buy-now">
-    <a
-      href="https://themeselection.com/item/sneat-dashboard-pro-bootstrap/"
-      target="_blank"
-      class="btn btn-danger btn-buy-now">Upgrade to Pro</a>
-  </div>
-
   <!-- Core JS -->
 
-  <script src="assets/vendor/libs/jquery/jquery.js"></script>
+  <script src="../assets/vendor/libs/jquery/jquery.js"></script>
 
-  <script src="assets/vendor/libs/popper/popper.js"></script>
-  <script src="assets/vendor/js/bootstrap.js"></script>
+  <script src="../assets/vendor/libs/popper/popper.js"></script>
+  <script src="../assets/vendor/js/bootstrap.js"></script>
 
-  <script src="assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+  <script src="../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
 
-  <script src="assets/vendor/js/menu.js"></script>
+  <script src="../assets/vendor/js/menu.js"></script>
 
   <!-- endbuild -->
 
   <!-- Vendors JS -->
-  <script src="assets/vendor/libs/apex-charts/apexcharts.js"></script>
+  <script src="../assets/vendor/libs/apex-charts/apexcharts.js"></script>
 
   <!-- Main JS -->
 
-  <script src="assets/js/main.js"></script>
+  <script src="../assets/js/main.js"></script>
 
   <!-- Page JS -->
-  <script src="assets/js/dashboards-analytics.js"></script>
+  <script src="../assets/js/dashboards-analytics.js"></script>
 
   <!-- Place this tag before closing body tag for github widget button. -->
   <script async defer src="https://buttons.github.io/buttons.js"></script>
---
+++ views/prediksi.php
@@ -0,0 +1,189 @@
+<!doctype html>
+
+<html
+  lang="en"
+  class="layout-menu-fixed layout-compact"
+  data-../assets-path="../assets/"
+  data-template="vertical-menu-template-free">
+
+<head>
+  <meta charset="utf-8" />
+  <meta
+    name="viewport"
+    content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
+
+  <title>Demo: Dashboard - Analytics | Sneat - Bootstrap Dashboard FREE</title>
+
+  <meta name="description" content="" />
+
+  <!-- Favicon -->
+  <link rel="icon" type="image/x-icon" href="../assets/img/favicon/favicon.ico" />
+
+  <!-- Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+    rel="stylesheet" />
+
+  <link rel="stylesheet" href="../assets/vendor/fonts/iconify-icons.css" />
+
+  <!-- Core CSS -->
+  <!-- build:css ../assets/vendor/css/theme.css  -->
+
+  <link rel="stylesheet" href="../assets/vendor/css/core.css" />
+  <link rel="stylesheet" href="../assets/css/demo.css" />
+
+  <!-- Vendors CSS -->
+
+  <link rel="stylesheet" href="../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
+
+  <!-- endbuild -->
+
+  <link rel="stylesheet" href="../assets/vendor/libs/apex-charts/apex-charts.css" />
+
+  <!-- Page CSS -->
+
+  <!-- Helpers -->
+  <script src="../assets/vendor/js/helpers.js"></script>
+  <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
+
+  <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
+
+  <script src="../assets/js/config.js"></script>
+</head>
+
+<body>
+  <!-- Layout wrapper -->
+  <div class="layout-wrapper layout-content-navbar">
+    <div class="layout-container">
+      <!-- Menu -->
+      <?php include 'partials/sidebar.html'; ?>
+      <!-- / Menu -->
+
+      <!-- Layout container -->
+      <div class="layout-page">
+        <!-- Navbar -->
+        <?php include 'partials/navbar.html'; ?>
+        <!-- / Navbar -->
+
+        <!-- Content wrapper -->
+        <div class="content-wrapper">
+          <!-- Content -->
+          <div class="container-xxl flex-grow-1 container-p-y">
+            <div class="row g-6 mb-6">
+              <!-- Basic -->
+              <div class="col-md-6">
+                <div class="card">
+                  <h5 class="card-header">Prediksi Potensi Stunting</h5>
+                  <div class="card-body demo-vertical-spacing demo-only-element">
+                    <div class="input-group">
+                      <span class="input-group-text" id="basic-addon11">Nama Ibu</span>
+                      <input
+                        type="text"
+                        class="form-control"
+                        placeholder="Nama Lengkap"
+                        aria-label="Nama Lengkap"
+                        aria-describedby="basic-addon11" />
+                    </div>
+
+                    <div class="input-group">
+                      <span class="input-group-text" id="basic-addon11">Usia Ibu</span>
+                      <input
+                        type="text"
+                        class="form-control"
+                        placeholder="Usia Ibu"
+                        aria-label="Usia Ibu"
+                        aria-describedby="basic-addon11" />
+                    </div>
+
+                    <div class="form-password-toggle">
+                      <label class="form-label" for="basic-default-password12">Tinggi Badan</label>
+                      <div class="input-group">
+                        <input
+                          type="text"
+                          class="form-control"
+                          id="basic-default-password12"
+                          placeholder="Masukkan Tinggi Badan Ibu"
+                          aria-describedby="basic-default-password2" />
+                        <span class="input-group-text" id="basic-addon13">cm</span>
+                      </div>
+                    </div>
+
+                    <div class="form-password-toggle">
+                      <label class="form-label" for="basic-default-password12">LILA</label>
+                      <div class="input-group">
+                        <input
+                          type="text"
+                          class="form-control"
+                          id="basic-default-password12"
+                          placeholder="Masukkan Lingkar Lengan Atas"
+                          aria-describedby="basic-default-password2" />
+                        <span class="input-group-text" id="basic-addon13">cm</span>
+                      </div>
+                    </div>
+
+                    <div class="form-password-toggle">
+                      <label class="form-label" for="basic-default-password12">Kadar Hemoglobin</label>
+                      <div class="input-group">
+                        <input
+                          type="text"
+                          class="form-control"
+                          id="basic-default-password12"
+                          placeholder="Masukkan Kadar Hemoglobin"
+                          aria-describedby="basic-default-password2" />
+                        <span class="input-group-text" id="basic-addon13">g/Dl</span>
+                      </div>
+                    </div>
+
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- / Content -->
+
+          <!-- Footer -->
+          <?php include 'partials/footer.html'; ?>
+          <!-- / Footer -->
+
+          <div class="content-backdrop fade"></div>
+        </div>
+        <!-- Content wrapper -->
+      </div>
+      <!-- / Layout page -->
+    </div>
+
+    <!-- Overlay -->
+    <div class="layout-overlay layout-menu-toggle"></div>
+  </div>
+  <!-- / Layout wrapper -->
+
+  <!-- Core JS -->
+
+  <script src="../assets/vendor/libs/jquery/jquery.js"></script>
+
+  <script src="../assets/vendor/libs/popper/popper.js"></script>
+  <script src="../assets/vendor/js/bootstrap.js"></script>
+
+  <script src="../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+
+  <script src="../assets/vendor/js/menu.js"></script>
+
+  <!-- endbuild -->
+
+  <!-- Vendors JS -->
+  <script src="../assets/vendor/libs/apex-charts/apexcharts.js"></script>
+
+  <!-- Main JS -->
+
+  <script src="../assets/js/main.js"></script>
+
+  <!-- Page JS -->
+  <script src="../assets/js/dashboards-analytics.js"></script>
+
+  <!-- Place this tag before closing body tag for github widget button. -->
+  <script async defer src="https://buttons.github.io/buttons.js"></script>
+</body>
+
+</html>
